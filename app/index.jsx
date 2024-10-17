6b2c80a5723e8ec4039b885c68a3672fc883ae2d
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-10-18 00:20:11 +0600

    project init

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text>Hello Bro</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
